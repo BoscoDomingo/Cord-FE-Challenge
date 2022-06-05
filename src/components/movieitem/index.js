7f6e132bca80ea2a1ca263b5d3f712b18afa4593
Bosco Domingo <boscodomingob@gmail.com>
2022-06-05 15:55:01 +0200

    Add picture, genres and title to MovieItem

--- src/components/movieitem/index.js
+++ src/components/movieitem/index.js
@@ -1,17 +1,34 @@
 import React from "react";
 import styled from 'styled-components';
 
-export default function MovieItem ({ movie, genres }) {
-  return (
-    // TODO: Complete the MovieItem component
-    <MovieItemWrapper>
-      <LeftCont>
-      </LeftCont>
-      <RightCont>
-        <Title>{movie.title}</Title>
-      </RightCont>
-    </MovieItemWrapper>
-  )
+import * as colors from "../../colors";
+
+export default function MovieItem({ movie, genres }) {
+    const postersBaseURL = "https://image.tmdb.org/t/p/w500";
+    console.log(movie.genre_ids, genres);
+
+    let genreNames = [];
+    for (let id of movie.genre_ids) {
+        genreNames.push(genres.find(genre => genre.id === id).name);
+    }
+
+    console.log(genreNames)
+    return (
+        // TODO: Complete the MovieItem component
+        <MovieItemWrapper>
+            <LeftCont>
+                <MoviePoster src={`${postersBaseURL}${movie.poster_path}`} alt={movie.title} />
+            </LeftCont>
+            <RightCont>
+                <Title>{movie.title}</Title>
+                <Genres>{genreNames.join(" | ")}</Genres>
+                
+                {/* Rating */}
+                {/* Summary */}
+                {/* Release date */}
+            </RightCont>
+        </MovieItemWrapper>
+    )
 }
 
 const MovieItemWrapper = styled.div`
@@ -20,6 +37,7 @@ const MovieItemWrapper = styled.div`
   border-radius: 5px;
   padding: 20px;
   margin: 15px 0;
+  display: flex;
 `
 
 const LeftCont = styled.div`
@@ -28,8 +46,32 @@ const LeftCont = styled.div`
 
 const RightCont = styled.div`
   display: inline-block;
+  align-items: left;
 `
 
+const MoviePoster = styled.img`
+    width: 35%;
+    height: auto;
+    object-fit: cover;
+    `
+
 const Title = styled.h2`
+  font-size: 1.4;
+`;
+
+const Genres = styled.h4`
+  font-size: 1;
+  color: ${colors.primaryColor};
+`;
+
+const Summary = styled.p`
+
+`;
+
+const Rating = styled.div`
+  font-size: 1.4;
+  color: ${colors.primaryColor};
+`;
+const ReleaseDate = styled.p`
   font-size: 1.4;
 `;
