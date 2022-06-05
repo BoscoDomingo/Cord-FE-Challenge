35ee3747a0bcb8b98af90f6e7533130402b167f1
Bosco Domingo <boscodomingob@gmail.com>
2022-06-05 13:27:30 +0200

    Fix fetcher to use async/await correctly

--- src/fetcher.js
+++ src/fetcher.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 // const API_KEY = process.env.MOVIES_DB_API_KEY || "991724e209b5167debbf1d8adaf43bb9"; // This would be the way to do it in prod
 // const BASE_URL = process.env.MOVIES_DB_BASE_URL || "https://api.themoviedb.org/3";
 
-const API_KEY = "991724e209b5167debbf1d8adaf43bb9"; // I will hardcode them for simplicity
+const API_KEY = "991724e209b5167debbf1d8adaf43bb9"; // I hardcoded them for simplicity
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const fetchMovies = async (query, year) => {
@@ -14,19 +14,26 @@ export const fetchMovies = async (query, year) => {
         console.error("Query must be provided");
         return {};
     }
-    return await axios.get(`${BASE_URL}/search/movie?api_key=${API_KEY}${query ? `&query=${query}` : ""}${year ? `&year=${year}` : ""}`)
-        .then(res => res.data)
-        .catch(err => console.error(err));
+    try {
+        return (await axios.get(`${BASE_URL}/search/movie?api_key=${API_KEY}${query ? `&query=${query}` : ""}${year ? `&year=${year}` : ""}`)).data
+    } catch (error) { // This error handling can be greatly improved
+        console.error(error);
+    }
+
 }
 
 export const fetchPopularMovies = async () => {
-    return await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
-        .then(res => res.data)
-        .catch(err => console.error(err));
+    try {
+        return (await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)).data;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 export const fetchMovieGenres = async () => {
-    return await axios.get(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`)
-        .then(res => res.data.genres)
-        .catch(err => console.error(err));
+    try {
+        return (await axios.get(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`)).data.genres;
+    } catch (error) {
+        console.error(error);
+    }
 }
